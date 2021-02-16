fb48bc682a5f22a0f872b65f042140fe9f07668f
Connie Ho <connie3@ualberta.ca>
2021-02-16 01:21:36 +0000

    addded login and logout features

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
 
@@ -11,61 +12,75 @@ const urlDatabase = {
 
 //functions
 function generateRandomString() {
-  return Math.random().toString(36).substr(2,6)
+  return Math.random().toString(36).substr(2,6);
 }
 
 //view engine
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 // middleware
-app.use(morgan('dev')) //(req,res,next) => {}
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(morgan('dev')); //(req,res,next) => {}
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 // get requests
-// app.get("/", (req, res) => {
-//   res.send("Hello!"); 
-// });
+app.get("/", (req, res) => {
+  res.redirect('/urls');
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = {
+    username: req.cookies['username'],
     urls: urlDatabase
-  }
+  };
   // console.log(req.body)
   // res.send('ok')
-  res.render('urls_index', templateVars)
+  res.render('urls_index', templateVars);
 });
 
-app.get('/urls/new', (req,res) => {
-  res.render('urls_new')
-})
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { 
-    shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL] 
+  const templateVars = {
+    username: req.cookies['username'],
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
 });
 
 app.get('/u/:shortURL', (req, res) => {
-  res.redirect(urlDatabase[req.params.shortURL])
-})
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
 
 app.post('/urls',(req,res)=>{
-  let shortURL = generateRandomString()
-  urlDatabase[shortURL] = req.body.longURL
-  res.redirect(`/urls/${shortURL}`)
-})
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
 
 app.post('/urls/:shortURL/delete',(req,res)=>{
-  delete urlDatabase[req.params.shortURL]
-  res.redirect(`/urls`)
-})
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+});
 
 app.post('/urls/:shortURL',(req,res)=>{
-  urlDatabase[req.params.shortURL] = req.body.longURL
-  res.redirect(`/urls`)
-})
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect(`/urls`);
+});
+
+app.post('/login',(req, res)=>{
+  res.cookie('username', req.body.username);
+  res.redirect(`/urls`);
+});
+
+app.post('/logout',(req, res)=>{
+  res.clearCookie('username');
+  res.redirect(`/urls`);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server connect ${PORT}!`);
